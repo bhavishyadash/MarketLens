a03d1623aff7a73074ef924b0a3f189d989c797b
Fiercedarkvenom <db220605@gmail.com>
2026-03-07 01:41:54 -0800

    Add Firebase to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
 }
 
 val localProps = Properties().apply {
@@ -66,6 +67,7 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
 
+    implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
