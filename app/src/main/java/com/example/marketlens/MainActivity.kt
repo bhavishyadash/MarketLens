46c5b2e44afb323557fe7a60e7e6402a7e99ebf5
Fiercedarkvenom <db220605@gmail.com>
2026-03-15 02:43:16 -0700

    Initialize and pass AuthViewModel to AppScaffold

--- app/src/main/java/com/example/marketlens/MainActivity.kt
+++ app/src/main/java/com/example/marketlens/MainActivity.kt
@@ -3,18 +3,27 @@ package com.example.marketlens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.navigation.compose.rememberNavController
 import com.example.marketlens.navigation.AppScaffold
 import com.example.marketlens.ui.theme.MarketLensTheme
+import com.example.marketlens.viewmodel.AuthViewModel
 
 class MainActivity : ComponentActivity() {
+
+
+    private val authViewModel: AuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             MarketLensTheme {
                 val navController = rememberNavController()
-                AppScaffold(navController = navController)
+                AppScaffold(
+                    navController = navController,
+                    authViewModel = authViewModel
+                )
             }
         }
     }
